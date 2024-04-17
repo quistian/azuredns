f1d48d36ba4d717734d3daae991fe62469d1fe6f
Russell Sutherland <russell.sutherland@utoronto.ca>
2024-04-17 16:20:45 -0400

    logging changes to octo-doit and added fetch markdown

--- octo-doit.sh
+++ octo-doit.sh
@@ -5,6 +5,10 @@ CHANGED_ZONES=/tmp/azure-changed-zones.$$
 
 LOG="./bc-current-scan.log"
 LOGDIR="./logs"
+LAST_CHANGED=$LOGDIR/last_changed.log
+SNAPSHOT=$LOGDIR/snapshot.log
+LAST_RRs=$LOGDIR/last_rrs.log
+
 TSTAMP_START=`date +"%Y-%m-%dT%H-%M-%S"`
 TSTAMP=$TSTAMP_START
 
@@ -27,10 +31,6 @@ fi
 
 echo $TSTAMP_START > $LOG
 echo "Scanning BC zones for changes" | tee -a $LOG
-
-LAST_CHANGED=$LOGDIR/last_changed.log
-SNAPSHOT=$LOGDIR/snapshot.log
-LAST_RRs=$LOGDIR/last_rrs.log
 
 echo 'Current BC Zone Data' >> $LOG
 azurecli dump "." | tr '[A-Z]' '[a-z]' | sort | tee $SNAPSHOT >> $LOG
@@ -57,49 +57,55 @@ if ! cmp -s $SNAPSHOT $LAST_RRs; then
         print j "."
     }' | sort | uniq > $LEAF_CHANGED_ZONES
 
-# cat $LEAF_CHANGED_ZONES
 # 237.privatelink.openai.azure.com.
 # 278.privatelink.openai.azure.com.
-    exit
 
     for leaf in `cat $LEAF_CHANGED_ZONES`
     do
         zdot=`echo $leaf | awk '{print(substr($0, index($0,".")+1))}'`
         echo $zdot >> $CHANGED_ZONES
-        echo "processing $leaf | tee -a $LOG
+        echo "processing $leaf" | tee -a $LOG
         echo "syncing BC auth to  BC v2" | tee -a $LOG
         octodns-sync --log-stream-stdout --config-file config/bcv1_to_bcv2.yml $leaf --doit | tee -a $LOG
     done
 
-    for zone in `cat $CHANGED_ZONES | sort | uniq`
+    for zdot in `cat $CHANGED_ZONES | sort | uniq`
     do
-        echo "processing $zone | tee -a $LOG
+        echo "processing $zdot" | tee -a $LOG
         echo "syncing BC v2 and merging leaf zones to local Yaml" | tee -a $LOG
-        octodns-sync --log-stream-stdout --config-file config/bcv2_merge_to_yaml.yml $zone --doit | tee -a $LOG
+        octodns-sync --log-stream-stdout --config-file config/bcv2_merge_to_yaml.yml $zdot --doit | tee -a $LOG
         echo "syncing merged Yaml to QA Yaml" | tee -a $LOG
-        octodns-sync --log-stream-stdout --config-file config/merged2qa.yaml $zone --doit | tee -a $LOG
+        octodns-sync --log-stream-stdout --config-file config/merged2qa.yaml $zdot --doit | tee -a $LOG
         echo "syncing merged Yaml  to PROD Yaml" | tee -a $LOG
-        octodns-sync --log-stream-stdout --config-file config/merged2prod.yaml $zone --doit | tee -a $LOG
+        octodns-sync --log-stream-stdout --config-file config/merged2prod.yaml $zdot --doit | tee -a $LOG
         echo "syncing QA Yaml to Azure QA Tennant" | tee -a $LOG
-        octodns-sync --log-stream-stdout --config-file config/qa2azure.yaml $zone --force --doit | tee -a $LOG
+        octodns-sync --log-stream-stdout --config-file config/qa2azure.yaml $zdot --force --doit | tee -a $LOG
         echo "syncing Prod Yaml to Azure Prod Tennant" | tee -a $LOG
-        octodns-sync --log-stream-stdout --config-file config/prod2azure.yaml $zone --doit >> $LOG
+        octodns-sync --log-stream-stdout --config-file config/prod2azure.yaml $zdot --doit >> $LOG
+        zone=`echo $zdot | sed -e 's/.$//'`
+        echo "generating unbound data for local pub to priv CNAMEs for zone $zone" | tee -a $LOG
+        sh -x gen-unbound-zone-data.sh $zone | tee -a $LOG
     done
 
-    echo "generating new unbound data for local pub to priv CNAMEs" | tee -a $LOG
-    sh -x gen-unbound-zone-data.sh $zone | tee -a $LOG
     # Restart unbound and dnsdist for any changes
     doas -u ansible ansible-playbook -t vars,unbound-data -l dns1,dns4,dns5 ~ansible/systems/unbound.yaml | tee -a $LOG
     # doas -u ansible ansible-playbook -v -t vars,dnsdist-data -l dns1,dns4,dns5 ~ansible/systems/dns.yaml | tee -a $LOG
+
+    TSTAMP_STOP=`date +"%Y-%m-%dT%H-%M-%S"`
+    echo $TSTAMP_STOP >> $LOG
+    SCANF="scan_${TSTAMP_START}_${TSTAMP_STOP}.log"
+    TSTF="${LOGDIR}/$SCANF"
+    cp $LOG $TSTF
+    rm $LAST_CHANGED
+    cd $LOGDIR
+    ln -s `basename $TSTF` `basename $LAST_CHANGED`
 else
-    echo "No Changes" >> $LOG
+    echo "No Changes to be made" | tee -a $LOG
+    TSTAMP_STOP=`date +"%Y-%m-%dT%H-%M-%S"`
+    echo $TSTAMP_STOP >> $LOG
+    SCANF="scan_${TSTAMP_START}_${TSTAMP_STOP}.log"
+    TSTF="${LOGDIR}/$SCANF"
+    cat $LOG | grep -v '~10.14' >  $TSTF
 fi
 
-TSTAMP_STOP=`date +"%Y-%m-%dT%H-%M-%S"`
-echo $TSTAMP_STOP >> $LOG
-TSTF="${LOGDIR}/scan_${TSTAMP_START}_${TSTAMP_STOP}.log"
-cp $LOG $TSTF
-rm $LAST_CHANGED
-ln -s $TSTF $LAST_CHANGED
-
-rm -f $LEAF_CHANGED_ZONES
+rm -f $LEAF_CHANGED_ZONES $CHANGED_ZONES
---
+++ src/azuredns/fetch-azure-priv-pub-markdown.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+
+URL='https://raw.githubusercontent.com/MicrosoftDocs/azure-docs/main/articles/private-link/private-endpoint-dns.md'
+
+curl $URL
